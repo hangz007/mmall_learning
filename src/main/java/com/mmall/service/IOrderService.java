3df2a1bf76cb917f06f10469c8b20b985b7d0277
hangz007 <hangz00749@163.com>
2018-04-22 09:15:29 +0800

    模块开发支付

---
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -0,0 +1,18 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+
+import java.util.Map;
+
+/**
+ * Created by shenzx on 2018/4/18.
+ */
+public interface IOrderService {
+
+    ServerResponse pay(Long orderNo, Integer userId, String path);
+
+    ServerResponse aliCallback(Map<String,String> params);
+
+    ServerResponse queryOrderPayStatus(Integer userId,Long orderNo);
+
+}
