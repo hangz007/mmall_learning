5de56e748ebc8cbad566d56fe1fbe9003630c98a
hangz007 <hangz00749@163.com>
2019-01-28 00:08:18 +0800

    自定义实现Redis分布式锁

--- src/main/java/com/mmall/util/RedisShardedPoolUtil.java
+++ src/main/java/com/mmall/util/RedisShardedPoolUtil.java
@@ -48,6 +48,21 @@ public class RedisShardedPoolUtil {
         return result;
     }
 
+    public static String getSet(String key,String value) {
+        ShardedJedis jedis = null;
+        String result = null;
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.getSet(key, value);
+        } catch (Exception e) {
+            log.error("getSet key:{} value:{} error",key,value,e);
+            RedisShardedPool.returnBrokenResource(jedis);
+            return  result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
     public static String set(String key,String value) {
         ShardedJedis jedis = null;
         String result = null;
@@ -56,6 +71,21 @@ public class RedisShardedPoolUtil {
             result = jedis.set(key, value);
         } catch (Exception e) {
             log.error("set key:{} value:{} error",key,value,e);
+            RedisShardedPool.returnBrokenResource(jedis);
+            return  result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
+    public static Long setnx(String key,String value) {
+        ShardedJedis jedis = null;
+        Long result = null;
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.setnx(key, value);
+        } catch (Exception e) {
+            log.error("setnx key:{} value:{} error",key,value,e);
             RedisShardedPool.returnBrokenResource(jedis);
             return  result;
         }
